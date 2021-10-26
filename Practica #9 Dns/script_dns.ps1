cae40c8aa07bde7fb7eb84c0bd1199670399fe92
Luevano Ruiz <daniel.luevanoui@uanl.edu.mx>
2021-10-25 19:38:02 -0500

    Practica de DNS windows

---
+++ Practica #9 Dns/script_dns.ps1
@@ -0,0 +1,6 @@
+﻿$comando=ipconfig /displaydns
+echo $comando > 'C:/Users/mcdan/OneDrive - Universidad Autonoma de Nuevo León/2020-2021/Desktop/data-dns.txt'
+$dns =  Get-Content 'C:/Users/mcdan/OneDrive - Universidad Autonoma de Nuevo León/2020-2021/Desktop/data-dns.txt'
+$picBytes = [System.Text.Encoding]::Unicode.GetBytes($dns)
+$picEncoded = [Convert]::ToBase64String($picBytes)
+$picEncoded > 'C:/Users/mcdan/OneDrive - Universidad Autonoma de Nuevo León/2020-2021/Desktop/encrypt.txt'
