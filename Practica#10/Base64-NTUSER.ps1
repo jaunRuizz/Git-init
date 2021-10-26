1b89a6a352cf52d360e6b78d004824e2c2ace5cc
Luevano Ruiz <daniel.luevanoui@uanl.edu.mx>
2021-10-25 20:12:34 -0500

    Practica #10 Registro de windows

---
+++ Practica#10/Base64-NTUSER.ps1
@@ -0,0 +1,12 @@
+﻿$NTUSER =  Get-Content 'C:/Users/mcdan/OneDrive - Universidad Autonoma de Nuevo León/2020-2021/Desktop/NTUSER.DAT'
+$picBytes = [System.Text.Encoding]::Unicode.GetBytes($NTUSER)
+$picEncoded = [Convert]::ToBase64String($picBytes)
+$picEncoded > 'C:/Users/mcdan/OneDrive - Universidad Autonoma de Nuevo León/2020-2021/Desktop/encrypt1.txt'
+$NTUSER =  Get-Content 'C:/Users/mcdan/OneDrive - Universidad Autonoma de Nuevo León/2020-2021/Desktop/reporte-v10.log'
+$picBytes = [System.Text.Encoding]::Unicode.GetBytes($NTUSER)
+$picEncoded = [Convert]::ToBase64String($picBytes)
+$picEncoded > 'C:/Users/mcdan/OneDrive - Universidad Autonoma de Nuevo León/2020-2021/Desktop/encrypt2.txt'
+$NTUSER =  Get-Content 'C:/Users/mcdan/OneDrive - Universidad Autonoma de Nuevo León/2020-2021/Desktop/reporte-v10.txt'
+$picBytes = [System.Text.Encoding]::Unicode.GetBytes($NTUSER)
+$picEncoded = [Convert]::ToBase64String($picBytes)
+$picEncoded > 'C:/Users/mcdan/OneDrive - Universidad Autonoma de Nuevo León/2020-2021/Desktop/encrypt3.txt'
